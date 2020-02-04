c4856d130f207217ea6a7e19ad8f8a4ce1d3c20e
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-02-04 16:33:53 +1100

    platform added

---
+++ Assets/Scripts/BrickManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickManager : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public WallManager wall;
+    public bool intact = true;
+    Vector2 oldPos;
+    public Quaternion originalRotationValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPos = transform.position;
+        originalRotationValue = gameObject.transform.rotation;
+
+    }
+    public void Collapse()
+    {
+
+        transform.Translate(0, 1);
+        rigid.constraints = RigidBody2Dconstraints.none;
+        intact = false;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+
+
+        if (other.tag == "Spike")
+        {
+
+            Collapse();
+
+        }
+
+
+
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (wall.collapse == false && intact == false)
+        {
+            intact = true;
+            transform.rotation = originalRotationValue;
+            transform.position = oldPos;
+            rigid.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationZ;
+
+        }
+    }
+}
---
+++ Assets/Scripts/WallManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallManager : MonoBehaviour
+{
+    public float maxHealth, curHealth, repair, material;
+    
+    public GameObject self;
+    public bool collapse = false;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (HealthBar.value != curHealth/maxHealth)
+        {
+            HealthBar.value = curHealth;
+        }
+        if(curHealth <= 0f)
+        {
+
+           
+            collapse = true;
+           
+            
+
+        }
+        if(curHealth == maxHealth + 1)
+        {
+            
+        }
+        if(curHealth >= maxHealth)
+        {
+            collapse = false;
+            
+
+        }
+
+    }
+}
