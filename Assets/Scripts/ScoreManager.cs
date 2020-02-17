1d89cc8d8c130e9a0216b2cd16046eb5799a83fc
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2020-02-17 15:52:53 +1100

    Settings change

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,9 +7,15 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     public static float distance;
+    public float highscore;
     public Text score;
+    public Text highscoreText;
     private void Update()
     {
         score.text = distance.ToString();
+        if(distance >= highscore)
+        {
+            highscoreText.text = distance.ToString();
+        }
     }
 }
