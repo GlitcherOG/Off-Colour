09196726ceefcce4aeb729897553982643d58e1d
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-02-19 12:15:06 +1100

    altering fireball

--- Assets/Scripts/Player/FireBall.cs
+++ Assets/Scripts/Player/FireBall.cs
@@ -32,7 +32,7 @@ public class FireBall : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag != "Player")
+        if (collision.tag != "Player" && collision.tag != "Platform")
         {
             Destroy(this.gameObject);
         }
