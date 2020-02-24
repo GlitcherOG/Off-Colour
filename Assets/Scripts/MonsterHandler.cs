b98ac044b8b4b03f498c9cb12d1e3f7f993abd1a
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-02-24 14:29:18 +1100

    added monster roar temp

---
+++ Assets/Scripts/MonsterHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterHandler : MonoBehaviour
+{
+    
+    public Animation anim;
+    public GameObject self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        if (anim.isPlaying == false)
+        {
+            self.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
